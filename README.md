0ed1b16e69de36e868b701eec160a0e76fe8d1ad
Nguyen Huu Thang <thangnh2001@gmail.com>
2023-05-17 00:47:31 +0700

    reinit base source

---
+++ README.md
@@ -0,0 +1,7 @@
+# Đồ án môn học: Thương mại điện tử
+cp .env.example .env
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
+```
+php artisan serve
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/blog-details', function () {
+    return view('blog-details');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/shop-details', function () {
+    return view('shop-details');
+});
+Route::get('/shop-grid', function () {
+    return view('shop-grid');
+});
+Route::get('/shoping-cart', function () {
+    return view('shoping-cart');
+});
+
