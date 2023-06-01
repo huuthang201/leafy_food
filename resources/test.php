6155eb55b209df41d9edc490b600581e7493e520
HoangTaam <19522159@gm.uit.edu.vn>
2023-06-01 22:42:36 +0700

    create db for product

---
+++ resources/test.php
@@ -0,0 +1,11 @@
+    [
+    'product_name' => 'Cà phê eatclean Tropicana Slim',
+    'category_id' => 1,
+    'price' => 69000,
+    'image' => ''
+    'description' => '',
+    'quantity' => 9,
+    'status' => 1,
+    'number' => 100,
+    'unit' => 'g'
+    ],
