45b80daae02e258fb39c3e741813e70e29dddf91
Nguyen Huu Thang <thangnh2001@gmail.com>
2023-06-11 23:48:28 +0700

    🪴[User Seeder]

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,6 +19,11 @@ class UserSeeder extends Seeder
                 'name' => 'Thắng Nguyễn Hữu',
                 'email' => 'admin@gmail.com',
                 'password' => Hash::make('123123123'),
+            ],
+            [
+                'name' => 'Hoàng Trí Tâm',
+                'email' => 'hoangtam704@gmail.com',
+                'password' => '$2y$10$KFMAyi0Di09nXlVu0w7t8.U4vsvp/l0svNhZxwNXdjFxOoPPXb.6m'
             ]
         ];
         foreach ($data as $item)
